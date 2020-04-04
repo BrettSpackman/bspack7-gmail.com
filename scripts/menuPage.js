399a4c4d3349df17f27be0a485ffb51e7aadfe63
Brett Spackman <brettspackman@Bretts-MacBook-Air.local>
2020-04-04 14:12:53 -0600

    Brickout working

---
+++ scripts/menuPage.js
@@ -0,0 +1,32 @@
+miniGame.pages['menuPage'] = (function(screens) {
+
+	function initialize() {
+		//
+		// Setup each of menu events for the screens
+		document.getElementById('id-new-game').addEventListener(
+			'click',
+			function() {screens.showScreen('gamePage'); });
+
+		document.getElementById('id-high-scores').addEventListener(
+			'click',
+			function() { screens.showScreen('page-highscores'); });
+
+		document.getElementById('id-help').addEventListener(
+			'click',
+			function() { screens.showScreen('page-help'); });
+
+		document.getElementById('id-about').addEventListener(
+			'click',
+			function() { screens.showScreen('page-about'); });
+	}
+
+	function run() {
+		//
+		// I know this is empty, there isn't anything to do.
+	}
+
+	return {
+		initialize : initialize,
+		run : run
+	};
+}(miniGame.screens));
